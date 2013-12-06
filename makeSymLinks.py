b1c69fa70543e2b252d04db62c9b46f492d35e27
Vamsi Krishna Atluri <vamc19@gmail.com>
2013-12-06 11:40:23 +0530

    Displays option to overwrite.

--- makeSymLinks.py
+++ makeSymLinks.py
@@ -1,8 +1,17 @@
 import os
 
-file_names = [f for f in os.listdir(".") if os.path.isfile(f)]
+restricted_files = ["makeSymLinks.py", ".directory", ".git", ".gitignore"]
+
+file_names = [f for f in os.listdir(".") if f not in restricted_files]
+
 for file_name in file_names:
-    if file_name != "makeSymLinks.py":
-        destination_path = "~/"+"."+file_name
-        source_path = file_name
-        os.symlink(source_path, destination_path)
+    destination_path = os.path.join(os.getenv("HOME"), "."+file_name)
+    source_path = os.path.realpath(file_name)
+    if os.path.exists(destination_path):
+      option = raw_input("File %s already exists in %s. Overwite? (y/n)   " % (file_name, destination_path))
+      if option.lower() == 'y' or option.lower() == "":
+	os.remove(destination_path)
+      else:
+	continue
+    print "Making symbolic link for", file_name
+    os.symlink(source_path, destination_path)
